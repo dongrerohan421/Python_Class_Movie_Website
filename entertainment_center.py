26a9b1333ade2d9efd0021a472d2049ddad65f4a
Rohan Dongre <dongrerohan421@gmail.com>
2014-12-26 17:20:34 -0600

    add instance of avatar

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,5 +4,10 @@ toy_story = media.Movie("Toy Story",
 			"A story of boy and his toys that come to life",
 			"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
 			"https://www.youtube.com/watch?v=0q1rxc96m2w")	# calling class Movie from media.py and assign to variable toy_story
+#print (toy_story.storyline)
 
-print (toy_story.storyline)
+avatar = media.Movie("Avatar",
+			"A marine on an alien planet",
+			"http://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+			"https://www.youtube.com/watch?v=cRdxXPV9GNQ")	# creates instance of avatar
+print (avatar.storyline)
