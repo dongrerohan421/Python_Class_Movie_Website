00018981ffe258de37ad9940c0d649956eacb376
Rohan Dongre <dongrerohan421@gmail.com>
2014-12-26 17:33:40 -0600

    add self keyword again

--- media.py
+++ media.py
@@ -1,6 +1,6 @@
 class Movie() :		# define class movie
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):		# creates and initializes space in memory
 		self.title = movie_title	# initialize movie variables
-		storyline = movie_storyline
+		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
