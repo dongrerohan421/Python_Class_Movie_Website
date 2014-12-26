ff72e887ba9a023373389032a7e8ded57589e40b
Rohan Dongre <dongrerohan421@gmail.com>
2014-12-26 17:00:31 -0600

    pass parameters of class Movie

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,3 +1,8 @@
 import media	# imports media package i.e all class, functions and objects
 
-toy_story = media.Movie()	# calling class Movie from media.py and assign to variable toy_story
+toy_story = media.Movie("Toy Story",
+			"A story of boy and his toys that come to life",
+			"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+			"https://www.youtube.com/watch?v=0q1rxc96m2w")	# calling class Movie from media.py and assign to variable toy_story
+
+print (toy_story.storyline)
