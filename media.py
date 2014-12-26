7024045b71f4d516423456f1537c9ddc25c41b47
Rohan Dongre <dongrerohan421@gmail.com>
2014-12-26 16:51:14 -0600

    define __init__ function

--- media.py
+++ media.py
@@ -1,1 +1,6 @@
-class Movie() : # define class movie
+class Movie() :		# define class movie
+	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):		# creates and initializes space in memory
+		self.title = movie_title	# initialize movie variables
+		self.storyline = movie_storyline
+		self.poster_image_url = poster_image
+		self.trailer_youtube_url = trailer_youtube
