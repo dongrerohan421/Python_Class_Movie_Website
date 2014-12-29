f32e11e9a60d2df9e954fe251cceda790ec9f14a
Rohan Dongre <dongrerohan421@gmail.com>
2014-12-29 01:36:39 -0600

    add ratings list and define documentation

--- media.py
+++ media.py
@@ -1,6 +1,9 @@
 import webbrowser
 
 class Movie() :		# define class movie
+	""" This class provides a way to store movie related information"""
+
+	VALID_RATINGS = ["G","PG","PG-13","R","G"]
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):		# creates and initializes space in memory
 		self.title = movie_title	# initialize movie variables
 		self.storyline = movie_storyline
