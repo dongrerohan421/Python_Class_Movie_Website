02c9a4a3ea52fde417c44d4ea7c819db29e51225
Rohan Dongre <dongrerohan421@gmail.com>
2014-12-29 04:24:52 -0600

    create child class

--- inheritance.py
+++ inheritance.py
@@ -4,5 +4,15 @@ class Parent() :	# created parent class
 		self.last_name = last_name	# assigning parameters to class variables
 		self.eye_color = eye_color
 
-billy_cyrus = Parent ("Cyrus","blue")	# passing values to parameter
-print (billy_cyrus.last_name)
+class Child(Parent) :	# create child class
+	def __init__(self, last_name, eye_color, number_of_toys):	# define child class init function
+		print("Child Constructor Called")
+		Parent.__init__(self,last_name, eye_color)
+		self.number_of_toys = number_of_toys
+
+#billy_cyrus = Parent ("Cyrus","blue")	# creating instance of parent class
+#print (billy_cyrus.last_name)
+
+miley_cyrus = Child("Cyrus", "Blue", 5)	# creating instance of child class
+print(miley_cyrus.last_name)
+print(miley_cyrus.number_of_toys)
